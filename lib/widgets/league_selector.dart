3ce4b3ecd1edf123f39a17645fffdbd9f7f4ebcd
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-24 09:55:48 +0100

    added the league selector and made it to include alot of leagues

--- lib/widgets/league_selector.dart
+++ lib/widgets/league_selector.dart
@@ -1,89 +1,130 @@
+// // lib/widgets/league_selector.dart
+// import 'package:flutter/material.dart';
+
+// class LeagueSelector extends StatefulWidget {
+//   @override
+//   _LeagueSelectorState createState() => _LeagueSelectorState();
+// }
+
+// // lib/widgets/league_selector.dart (continued)
+// class _LeagueSelectorState extends State<LeagueSelector> {
+//   int _selectedIndex = 0;
+
+//   final List<Map<String, dynamic>> _leagues = [
+//     {'name': 'All', 'icon': Icons.sports_soccer},
+//     {'name': 'Premier League', 'icon': Icons.emoji_events},
+//     {'name': 'La Liga', 'icon': Icons.emoji_events},
+//     {'name': 'Serie A', 'icon': Icons.emoji_events},
+//     {'name': 'Bundesliga', 'icon': Icons.emoji_events},
+//     {'name': 'Ligue 1', 'icon': Icons.emoji_events},
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       height: 90,
+//       child: ListView.builder(
+//         scrollDirection: Axis.horizontal,
+//         itemCount: _leagues.length,
+//         itemBuilder: (context, index) {
+//           final isSelected = _selectedIndex == index;
+//           return GestureDetector(
+//             onTap: () {
+//               setState(() {
+//                 _selectedIndex = index;
+//               });
+//             },
+//             child: Container(
+//               width: 70,
+//               margin: EdgeInsets.only(right: 12),
+//               decoration: BoxDecoration(
+//                 color:
+//                     isSelected
+//                         ? Theme.of(context).primaryColor.withOpacity(0.1)
+//                         : Colors.transparent,
+//                 borderRadius: BorderRadius.circular(8),
+//                 border: Border.all(
+//                   color:
+//                       isSelected
+//                           ? Theme.of(context).primaryColor
+//                           : Colors.grey.withOpacity(0.3),
+//                   width: 1,
+//                 ),
+//               ),
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Icon(
+//                     _leagues[index]['icon'],
+//                     color:
+//                         isSelected
+//                             ? Theme.of(context).primaryColor
+//                             : Colors.grey,
+//                     size: 28,
+//                   ),
+//                   SizedBox(height: 8),
+//                   Text(
+//                     _leagues[index]['name'],
+//                     style: TextStyle(
+//                       color:
+//                           isSelected
+//                               ? Theme.of(context).primaryColor
+//                               : Colors.grey[700],
+//                       fontSize: 12,
+//                       fontWeight:
+//                           isSelected ? FontWeight.bold : FontWeight.normal,
+//                     ),
+//                     textAlign: TextAlign.center,
+//                     maxLines: 2,
+//                     overflow: TextOverflow.ellipsis,
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
 // lib/widgets/league_selector.dart
 import 'package:flutter/material.dart';
+import 'package:livekick/models/odds.dart';
 
-class LeagueSelector extends StatefulWidget {
-  @override
-  _LeagueSelectorState createState() => _LeagueSelectorState();
-}
+class LeagueSelector extends StatelessWidget {
+  final List<LeagueInfo> leagues;
+  final int selectedLeagueId;
+  final Function(int) onLeagueSelected;
 
-// lib/widgets/league_selector.dart (continued)
-class _LeagueSelectorState extends State<LeagueSelector> {
-  int _selectedIndex = 0;
-
-  final List<Map<String, dynamic>> _leagues = [
-    {'name': 'All', 'icon': Icons.sports_soccer},
-    {'name': 'Premier League', 'icon': Icons.emoji_events},
-    {'name': 'La Liga', 'icon': Icons.emoji_events},
-    {'name': 'Serie A', 'icon': Icons.emoji_events},
-    {'name': 'Bundesliga', 'icon': Icons.emoji_events},
-    {'name': 'Ligue 1', 'icon': Icons.emoji_events},
-  ];
+  const LeagueSelector({
+    required this.leagues,
+    required this.selectedLeagueId,
+    required this.onLeagueSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 90,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: _leagues.length,
-        itemBuilder: (context, index) {
-          final isSelected = _selectedIndex == index;
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            child: Container(
-              width: 70,
-              margin: EdgeInsets.only(right: 12),
-              decoration: BoxDecoration(
-                color:
-                    isSelected
-                        ? Theme.of(context).primaryColor.withOpacity(0.1)
-                        : Colors.transparent,
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(
-                  color:
-                      isSelected
-                          ? Theme.of(context).primaryColor
-                          : Colors.grey.withOpacity(0.3),
-                  width: 1,
-                ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    _leagues[index]['icon'],
-                    color:
-                        isSelected
-                            ? Theme.of(context).primaryColor
-                            : Colors.grey,
-                    size: 28,
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    _leagues[index]['name'],
-                    style: TextStyle(
-                      color:
-                          isSelected
-                              ? Theme.of(context).primaryColor
-                              : Colors.grey[700],
-                      fontSize: 12,
-                      fontWeight:
-                          isSelected ? FontWeight.bold : FontWeight.normal,
-                    ),
-                    textAlign: TextAlign.center,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+    return DropdownButtonFormField<int>(
+      decoration: InputDecoration(
+        labelText: 'League',
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
       ),
+      value: selectedLeagueId > 0 ? selectedLeagueId : null,
+      hint: Text('Select League'),
+      onChanged: (value) {
+        if (value != null) {
+          onLeagueSelected(value);
+        }
+      },
+      items:
+          leagues.map((league) {
+            return DropdownMenuItem<int>(
+              value: league.id,
+              child: Text('${league.name} (${league.country})'),
+            );
+          }).toList(),
     );
   }
 }
--- lib/widgets/live_match_view.dart
+++ lib/widgets/live_match_view.dart
@@ -1,131 +1,90 @@
-// // lib/widgets/live_match_view.dart
-// // ignore_for_file: prefer_const_constructors, deprecated_member_use
+// lib/widgets/live_match_view.dart
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
-// import 'package:flutter/material.dart';
-// import 'package:livekick/models/match.dart';
-
-// class LiveMatchView extends StatelessWidget {
-//   final Match match;
-
-//   const LiveMatchView({required this.match});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-//       color: Colors.black.withOpacity(0.6),
-//       child: Row(
-//         children: [
-//           Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               Text(
-//                 match.leagueName,
-//                 style: TextStyle(color: Colors.white70, fontSize: 12),
-//               ),
-//               SizedBox(height: 4),
-//               Row(
-//                 children: [
-//                   Text(
-//                     match.homeTeam,
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 14,
-//                     ),
-//                   ),
-//                   Text(
-//                     ' vs ',
-//                     style: TextStyle(color: Colors.white70, fontSize: 14),
-//                   ),
-//                   Text(
-//                     match.awayTeam,
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 14,
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//           Spacer(),
-//           Container(
-//             padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-//             decoration: BoxDecoration(
-//               color: Colors.white.withOpacity(0.2),
-//               borderRadius: BorderRadius.circular(4),
-//             ),
-//             child: Text(
-//               '${match.homeScore} - ${match.awayScore}',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontWeight: FontWeight.bold,
-//                 fontSize: 16,
-//               ),
-//             ),
-//           ),
-//           SizedBox(width: 12),
-//           Container(
-//             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-//             decoration: BoxDecoration(
-//               color: Colors.red,
-//               borderRadius: BorderRadius.circular(4),
-//             ),
-//             child: Text(
-//               match.time,
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontWeight: FontWeight.bold,
-//                 fontSize: 12,
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// lib/widgets/league_selector.dart
 import 'package:flutter/material.dart';
-import 'package:livekick/models/odds.dart';
+import 'package:livekick/models/match.dart';
 
-class LeagueSelector extends StatelessWidget {
-  final List<LeagueInfo> leagues;
-  final int selectedLeagueId;
-  final Function(int) onLeagueSelected;
+class LiveMatchView extends StatelessWidget {
+  final Match match;
 
-  const LeagueSelector({
-    required this.leagues,
-    required this.selectedLeagueId,
-    required this.onLeagueSelected,
-  });
+  const LiveMatchView({required this.match});
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField<int>(
-      decoration: InputDecoration(
-        labelText: 'League',
-        border: OutlineInputBorder(),
-        contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      color: Colors.black.withOpacity(0.6),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                match.leagueName,
+                style: TextStyle(color: Colors.white70, fontSize: 12),
+              ),
+              SizedBox(height: 4),
+              Row(
+                children: [
+                  Text(
+                    match.homeTeam,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                  Text(
+                    ' vs ',
+                    style: TextStyle(color: Colors.white70, fontSize: 14),
+                  ),
+                  Text(
+                    match.awayTeam,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Spacer(),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Text(
+              '${match.homeScore} - ${match.awayScore}',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          SizedBox(width: 12),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Text(
+              match.time,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ],
       ),
-      value: selectedLeagueId > 0 ? selectedLeagueId : null,
-      hint: Text('Select League'),
-      onChanged: (value) {
-        if (value != null) {
-          onLeagueSelected(value);
-        }
-      },
-      items:
-          leagues.map((league) {
-            return DropdownMenuItem<int>(
-              value: league.id,
-              child: Text('${league.name} (${league.country})'),
-            );
-          }).toList(),
     );
   }
 }
